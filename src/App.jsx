ad23df6062a3105e81d1eeb53b6582c1c43feb48
Mine Cetinkaya <mine.cetinkaya@gmail.com>
2024-12-12 11:43:11 +0100

    navbar, not complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import { useState } from 'react'
 //import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer.jsx'
+import Navbar from './components/Navbar.jsx'
+
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div> hello      </div>
-
-
-
-      
+      <Navbar/>
       <Footer/>
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(){
+
+    return (<nav className="navbar">
+            <div className="navbar-brand">Ironhack Ecommerce</div>
+
+        </nav>);
+}
+
+export default Navbar;
