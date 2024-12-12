e24f760e60764e8599dad495f6a9b8a374d8b3ad
minecet <mine.cetinkaya@gmail.com>
2024-12-12 20:41:41 +0100

    correcting after merge artefacts

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Navbar from './components/Navbar.jsx'
-<<<<<<< HEAD
 import List from './components/List.jsx'
-=======
 import Sidebar from './components/Sidebar.jsx'
->>>>>>> 42603a91529e8d5cb1caaea030eb5c01c8807023
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -17,11 +14,8 @@ function App() {
   return (
     <>
       <Navbar/>
-<<<<<<< HEAD
       <List/>
-=======
       <Sidebar/>
->>>>>>> 42603a91529e8d5cb1caaea030eb5c01c8807023
       <Footer/>
     </>
   )
