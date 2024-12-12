be01de8f32c9cd1ec00db3aa4075367a415e0dfc
patricio <patoare@hotmail.com>
2024-12-12 14:53:40 +0100

    Sidebar starting

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar.jsx'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Sidebar/>
       <Footer/>
     </>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div>
+    <aside className="linkMenu">
+      <a> Home </a>
+      <a> About Page </a>      
+    </aside>
+    </div>
+  );
+};
+
+export default Sidebar;
